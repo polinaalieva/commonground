f90f34f7c1c38e19d26ce54da360577787034e31
Polina <polina.alieva.int@gmail.com>
2026-04-08 00:28:47 +0300

    header fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect, useRef } from 'react'
 import { useLocation } from 'react-router-dom'
 import { Link } from 'react-router-dom'
-import logo from '../assets/CG_logo_bw.svg'
 import './Header.css'
 import LangSwitcher from './ui/LangSwitcher'
 import BurgerButton from './ui/BurgerButton'
@@ -15,7 +14,7 @@ function Header({ isMap = false }) {
   const [menuOpen, setMenuOpen] = useState(false)
   const menuRef = useRef(null)
 
-  // Закрываем меню при клике вне него
+
   useEffect(() => {
     if (!menuOpen) return
     function handleClickOutside(e) {
@@ -31,11 +30,7 @@ function Header({ isMap = false }) {
     <header className="cg-header">
       <div className="cg-header__inner">
         <Link to={isRu ? '/ru' : '/'} className="cg-header__link">
-          <img className="cg-header__logo" src={logo} alt="Common Ground" />
-          <div className="cg-header__text">
             <div className="cg-header__title">Common Ground</div>
-            <div className="cg-header__subtitle">{c.subtitle_header}</div>
-          </div>
         </Link>
 
         {isMap ? (
