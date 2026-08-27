eff1bfdae39762f471255c7444c093bb05262232
Polina <polina.alieva.int@gmail.com>
2026-08-27 11:28:11 +0400

    legal update

--- src/config/content-menu.js
+++ src/config/content-menu.js
@@ -3,7 +3,7 @@ export const MENU_CONTENT = {
     about_btn: 'About map',
     about_text: 'Common Ground\n\nA shared map of how people experience places around them.',
     about_contact: {
-      prefix: 'Get in touch:',
+      prefix: 'Get in touch',
       links: [
         { text: 'hi@commonground.page', href: 'mailto:hi@commonground.page' },
       ],
@@ -18,7 +18,7 @@ export const MENU_CONTENT = {
     about_text: 'An interactive event map EventCG\nSearch sessions and exhibitors, tap any spot on the map to get there\n\nPowered by Common Ground\nA shared map of how people experience places around them, from city streets to venues',
     about_link: { match: 'EventCG', href: 'https://eventcg.co' },
     about_contact: {
-      prefix: 'Get in touch:',
+      prefix: 'Get in touch',
       links: [
         { text: 'hi@commonground.page', href: 'mailto:hi@commonground.page' },
       ],
